c55add0a2518199d70e05177c5be695c27b75e2e
jomi2003 <josequinchuela@yahoo.com>
2016-03-05 19:00:52 -0500

    Task 8 - Created the IOwnable interface using the Extract an Interface

---
+++ Monopoly/src/edu/towson/cis/cosc603/project2/monopoly/IOwnable.java
@@ -0,0 +1,57 @@
+package edu.towson.cis.cosc603.project2.monopoly;
+
+public interface IOwnable {
+
+	/**
+	 * Gets the name.
+	 *
+	 * @return the name
+	 */
+	String getName();
+
+	/**
+	 * Gets the theOwner.
+	 *
+	 * @return the theOwner
+	 */
+	Player getTheOwner();
+
+	/**
+	 * Gets the price.
+	 *
+	 * @return the price
+	 */
+	int getPrice();
+
+	/**
+	 * Play action.
+	 */
+	void playAction();
+
+	/**
+	 * Sets the theOwner.
+	 *
+	 * @param theOwner the new theOwner
+	 */
+	void setTheOwner(Player owner);
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	String toString();
+
+	/**
+	 * Checks if is available.
+	 *
+	 * @return true, if is available
+	 */
+	boolean isAvailable();
+
+	/**
+	 * Sets the available.
+	 *
+	 * @param available the new available
+	 */
+	void setAvailable(boolean available);
+
+}
