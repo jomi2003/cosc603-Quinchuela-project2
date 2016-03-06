7387298bd57ce7e3f5631cdd6437b8107153ec03
jomi2003 <josequinchuela@yahoo.com>
2016-03-06 16:49:32 -0500

    Task 11 - Changed playAction() method signature and adjusted classes

--- Monopoly/src/edu/towson/cis/cosc603/project2/monopoly/IOwnable.java
+++ Monopoly/src/edu/towson/cis/cosc603/project2/monopoly/IOwnable.java
@@ -25,8 +25,10 @@ public interface IOwnable {
 
 	/**
 	 * Play action.
+	 * @param msg TODO
+	 * @return TODO
 	 */
-	void playAction();
+	Boolean playAction(String msg);
 
 	/**
 	 * Sets the theOwner.
